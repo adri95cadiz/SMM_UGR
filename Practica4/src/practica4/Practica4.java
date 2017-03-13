c8e25e7de80adde4b392d2314320348b2b67520f
Adrián Portillo Sánchez <adri95cadiz@correo.ugr.es>
2017-03-13 22:25:19 +0100

    Ventana basica desarrollada.

---
+++ Practica4/src/practica4/Practica4.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica4;
+
+/**
+ *
+ * @author Adri
+ */
+public class Practica4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        VentanaPrincipal paint = new VentanaPrincipal();
+        paint.setVisible(true);
+    }
+    
+}
